d869e882829039930615b008b2f47c260232491d
Yogesh Dhande <yogeshkd@gmail.com>
2021-01-16 20:21:30 +0000

    Files on the cloud storage is now only updated in the server shutdown event

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,12 +2,4 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map",
   },
-  chainWebpack: (config) => {
-    config.module
-      .rule("node")
-      .test(/\.node$/)
-      .use("node-loader")
-      .loader("node-loader")
-      .end();
-  },
 };
--- server/src/api/models/blob.py
+++ server/src/api/models/blob.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from pydantic import BaseModel
+from services.cloud_storage import cloud_storage_service
 from services.storage import storage_service
 
 
@@ -9,7 +10,8 @@ class Blob(BaseModel):
     text: Optional[str] = None
 
     def reload(self, project_id):
-        self.text = storage_service.get_text_from_blob(project_id, self.relative_path)
+        if not self.text:
+            self.text = storage_service.get_text_from_blob(project_id, self.relative_path)
         return self
 
     def save(self, project_id) -> None:
@@ -17,11 +19,14 @@ class Blob(BaseModel):
             raise ValueError("Blob.save must be called with the text to be saved")
         storage_service.write_text_to_blob(project_id, self.relative_path, self.text)
 
+    def upload(self, project_id):
+        self.reload()
+        cloud_storage_service.write_text_to_blob(project_id, self.relative_path, self.text)
+
     def exists_locally(self, project_id) -> bool:
         return storage_service.does_blob_exist_locally(project_id, self.relative_path)
 
-    def download(self, project_id) -> None:
+    def ensure_locally(self, project_id) -> None:
         if not self.exists_locally(project_id):
-            self.reload(project_id)
+            self.text = cloud_storage_service.get_text_from_blob(project_id, self.relative_path)
             self.save(project_id)
-
