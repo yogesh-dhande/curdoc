249088327e0a44303336b67bdb9b0f8920c98a08
Yogesh Dhande <yogeshkd@gmail.com>
2021-12-05 17:48:30 -0800

    A base.yaml file is now served to show environment details

---
+++ client/pages/_userName/projects/_projectSlug/environment.vue
@@ -0,0 +1,17 @@
+<template>
+    <card class="bg-white">
+        <object
+            :data="`${$config.backendUrl}/static/envs/base.yaml`"
+            class="min-h-screen w-full"
+        >
+            Not supported
+        </object>
+    </card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
