1e5a18c3a3685cc379eadd2267ace8e4fbdc312a
Yogesh Dhande <yogeshkd@gmail.com>
2021-12-12 09:59:12 -0800

    Server run.sh now does not reload unless DEPLOY_TARGET=dev

--- client/store/index.js
+++ client/store/index.js
@@ -81,11 +81,9 @@ export const actions = {
       authUser.getIdToken(/* forceRefresh */ true).then((token) => {
         commit("SET_TOKEN", token);
         commit("SET_EMAIL_VERIFIED", authUser.emailVerified);
-        if (!authUser.emailVerified) {
-          this.$firebase.sendVerificationEmail(authUser);
-        }
 
         onSnapshot(doc(this.$firebase.db, "users", authUser.uid), (snap) => {
+          console.log("setting user data");
           commit("SET_USER", snap.data() || {});
         });
 
--- server/run.sh
+++ server/run.sh
@@ -1,7 +1,13 @@
 # !/bin/bash
 echo starting server
 cd lsp && node app.js &
-uvicorn app:app --port 8000 --reload-dir src/api --reload --app-dir src/api --host 0.0.0.0 &
+if [[ "$DEPLOY_TARGET" == dev ]]
+then
+    uvicorn app:app --port 8000 --reload-dir src/api --reload --app-dir src/api --host 0.0.0.0 &
+else
+    uvicorn app:app --port 8000 --app-dir src/api --host 0.0.0.0 &
+fi
+
 caddy run &
 
 # Wait for any process to exit
