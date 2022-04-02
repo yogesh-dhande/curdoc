6c674ccabc8d62efadde7248e4846dc1ad51f5ea
Yogesh Dhande <yogeshkd@gmail.com>
2022-04-02 09:53:10 -0700

    Change to bokeh-gcp-deploy

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,7 @@ PROJECT_ID=$(gcloud config get-value project)
 
 echo $PROJECT_ID
 
-gcloud deploy apply --file clouddeploy.yaml --region=us-central1 --project=$PROJECT_ID
-
-# gcloud builds submit --timeout=1200s
+# gcloud builds submit --timeout=1800s
 
 gcloud deploy apply --file clouddeploy.yaml --region=us-central1 --project=$PROJECT_ID
 
@@ -14,4 +12,6 @@ gcloud deploy releases create 'rel-$DATE-$TIME' \
   --project=$PROJECT_ID \
   --region=us-central1 \
   --delivery-pipeline=curdoc-deployment \
-  --images=frontend=gcr.io/$PROJECT_ID/frontend:latest,lsp=gcr.io/$PROJECT_ID/lsp:latest,sandbox=gcr.io/$PROJECT_ID/sandbox:latest
+  --images=frontend=us-docker.pkg.dev/$PROJECT_ID/gcr.io/frontend:latest,lsp=us-docker.pkg.dev/$PROJECT_ID/gcr.io/lsp:latest,sandbox=us-docker.pkg.dev/$PROJECT_ID/gcr.io/sandbox:latest
+
+# cd client && npm run deploy
